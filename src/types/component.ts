3a1e063e5538fe734985395dc6b6ba9e07120df7
hongmaoAI <1500839960@qq.com>
2024-01-07 14:10:24 +0800

    feat(component.ts) : add annotaion

--- src/types/component.ts
+++ src/types/component.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: hong
+ * @Date: 2024-01-07 11:49:34
+ * @LastEditTime: 2024-01-07 12:04:16
+ * @Description: 请填写简介
+ */
 export interface Component {
 	id: number // 组件唯一标识
 	name: string // 组件名称
@@ -11,6 +17,24 @@ export interface State {
 }
 
 export interface Action {
+	/**
+	 * 添加组件
+	 * @param component 组件属性
+	 * @param parentId 组件Id
+	 * @memberof Action
+	 */
 	addComponent: (component: Component, parentId?: number) => void
+	/**
+	 * 设置当前组件Id
+	 * @param componentId 组件Id
+	 * @memberof Action
+	 */
 	setCurComponentId: (componentId: number) => void
+	/**
+	 * 更新组件
+	 * @param componentId 组件Id
+	 * @param props 新组建属性
+	 * @memberof Action
+	 */
+	updateComponentProps: (componentId: number, props: any) => void
 }
